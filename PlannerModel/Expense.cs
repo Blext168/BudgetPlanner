d6a4cb2a0cf8a6882c60303961aeccdea6bd738d
Danilo Schiefer <danilo.schiefer@lit.de>
2025-10-29 16:20:20 +0100

    Moved all files

---
+++ PlannerModel/Expense.cs
@@ -0,0 +1,13 @@
+﻿namespace PlannerModel
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public bool OneTime { get; set; }
+        public DateTime? MonthOfExpense { get; set; }
+        public int DayInMonth { get; set; } = 1;
+        public int UserId { get; set; }
+    }
+}
